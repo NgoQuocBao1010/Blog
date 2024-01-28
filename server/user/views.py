b16a7041f0e78da49f49e1863d7451ec68b7ebdb
ngoquocbao1010 <totti.chelsea@gmail.com>
2024-01-28 23:12:28 +0700

    💡 feat: Catch invalid login credential error

--- server/user/views.py
+++ server/user/views.py
@@ -19,13 +19,13 @@ class ListRegisterView(generics.ListCreateAPIView):
         if self.request.method == "POST":
             return User.objects.all()
         return User.objects.exclude(pk=self.request.user.pk).order_by("-created_at")
-    
+
     def get_permissions(self):
         if self.request.method == "POST":
             return [AllowAny()]
-        
+
         return [IsAuthenticated()]
-    
+
     def get_serializer_class(self):
         if self.request.method == "POST":
             return UserRegisterSerializer
@@ -41,13 +41,18 @@ class LoginView(ObtainAuthToken):
         serializer = self.serializer_class(
             data=request.data, context={"request": request}
         )
-        serializer.is_valid(raise_exception=True)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except ValueError as error:
+            return Response({"error_message": "Invalid email or password"})
+
         user = serializer.validated_data["user"]
         token, created = Token.objects.get_or_create(user=user)
         return Response({"token": token.key, "user_id": user.pk, "email": user.email})
-    
 
-class UserDelteView(generics.DestroyAPIView):
+
+class UserDeleteView(generics.DestroyAPIView):
     queryset = User.objects.all()
     permission_classes = [IsAuthenticated]
     lookup_field = "email"
@@ -56,16 +61,14 @@ class UserDelteView(generics.DestroyAPIView):
         # Get the model instance
         instance: User = self.get_object()
 
-        
-
-        if (instance.email == request.user.email):
+        if instance.email == request.user.email:
             return Response(
-                {"detail": "Cannot delete your own account"}, status=status.HTTP_400_BAD_REQUEST
+                {"detail": "Cannot delete your own account"},
+                status=status.HTTP_400_BAD_REQUEST,
             )
-        
+
         # Simply delete - no need to instantiate the serializer
         self.perform_destroy(instance)
-        
 
         return Response(
             {"message": "Deleted Successfully"}, status=status.HTTP_204_NO_CONTENT

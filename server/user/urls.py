4e74c1d70aa30fb775cf556356e00f174ebdec10
ngoquocbao1010 <totti.chelsea@gmail.com>
2024-01-28 23:12:40 +0700

    🧑🏽‍💻: Update refactor, update requirements

--- server/user/urls.py
+++ server/user/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 
-from user.views import ListRegisterView, LoginView, UserDelteView
+from user.views import ListRegisterView, LoginView, UserDeleteView
 
 urlpatterns = [
     path("api/users", ListRegisterView.as_view(), name="user_list_register"),
     path("api/login", LoginView.as_view(), name="api_token_auth"),
-    path("api/users/<str:email>", UserDelteView.as_view(), name="user_delete"),
+    path("api/users/<str:email>", UserDeleteView.as_view(), name="user_delete"),
 ]
